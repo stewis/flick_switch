5c2414afac6fe5f5995f8889f99328bfc0815b90
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-04-19 11:03:52 +0100

    Degrades to checkbox, added test files

--- jquery.flick-switch.js
+++ jquery.flick-switch.js
@@ -5,17 +5,17 @@
 *  Date:   2/4/2008                             *
 ************************************************/
 
-jQuery.fn.flickSwitch = function(start_state, options) {
-
-	var state = start_state == 'on' ? start_state : 'off';
+jQuery.fn.flickSwitch = function(options) {
+	
+	var state = $(this).prop("checked") ? 'on' : 'off';
 	
 	// define default settings
 	var settings = {
 		mouse_over: 'pointer',
 		mouse_out:  'default',
-		switch_on_container_path: '/ajax-switch/flick_switch_container_on.png',
-		switch_off_container_path: '/ajax-switch/flick_switch_container_off.png',
-		switch_path: '/ajax-switch/flick_switch.png',
+		switch_on_container_path: 'flick_switch_container_on.png',
+		switch_off_container_path: 'flick_switch_container_off.png',
+		switch_path: 'flick_switch.png',
 		switch_height: 27,
 		switch_width: 94
 	};
@@ -37,28 +37,29 @@ jQuery.fn.flickSwitch = function(start_state, options) {
 		image = jQuery('<img class="flick_switch" style="height:'+settings.switch_height+'px; width:'+settings.switch_width+'px; background-image:url('+settings.switch_path+'); background-repeat:none; background-position:'+(state == 'on' ? 0 : -53)+'px" src="'+(state == 'on' ? settings.switch_on_container_path : settings.switch_off_container_path)+'" /></div>');
 
 		// insert into placeholder
-		jQuery(this).html(jQuery(container).html(jQuery(image)));
-
-		jQuery(this).mouseover(function(){
+		jQuery(this).css("display", "none");
+		jQuery(this).wrap(jQuery(container).html(jQuery(image)));
+		console.log($(this).parent().parent());
+		$(this).parent().parent().mouseover(function(){
 			jQuery(this).css("cursor", settings.mouse_over);
 		});
 
-		jQuery(this).mouseout(function(){
+		$(this).parent().parent().mouseout(function(){
 			jQuery(this).css("background", settings.mouse_out);
 		});
 
 		// click handling
-		jQuery(this).click(function() {
+		$(this).parent().parent().click(function() {
 			if(state == 'on') {
 				jQuery(this).find('.flick_switch').animate({backgroundPosition: -53}, "slow", function() {
 					jQuery(this).attr('src', settings.switch_off_container_path);
-					jQuery(this).trigger("off");
+					jQuery("input", this).attr("checked", false);
 				});
 				state = 'off';
 			}
 			else {
 				jQuery(this).find('.flick_switch').animate({backgroundPosition: 0}, "slow", function() {
-					jQuery(this).trigger("on");
+					jQuery("input", this).attr("checked", true);
 				});
 				jQuery(this).find('.flick_switch').attr('src', settings.switch_on_container_path);
 				state = 'on';
